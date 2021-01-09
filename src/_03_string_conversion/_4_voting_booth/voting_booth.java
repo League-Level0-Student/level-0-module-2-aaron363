93710ff1fc8d62f2da433e41989eeddad19d707e
root <root@de4f3e3666b9>
2021-01-09 20:56:23 +0000

    Level 0 module 2 Done

--- src/_03_string_conversion/_4_voting_booth/voting_booth.java
+++ src/_03_string_conversion/_4_voting_booth/voting_booth.java
@@ -1,9 +1,17 @@
 package _03_string_conversion._4_voting_booth;
 
+import javax.swing.JOptionPane;
+
 public class voting_booth {
 
 	public static void main(String[] args) {
-		
+		String ageString = JOptionPane.showInputDialog("What is your age?");
+		int age = Integer.parseInt(ageString);
+		if(age >= 18) {
+			JOptionPane.showInputDialog("Who should be the next president?");
+		}else {
+			JOptionPane.showMessageDialog(null, "Nobody cares what you think");
+		}
 
 	}
 
